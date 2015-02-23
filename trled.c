7860c7cfa91ea0f59814e9cbe6638a6703b05e1f
powturbo <powturbo@gmail.com>
2015-02-23 16:40:11 +0100

    TurboRLE - Run Length Encoding

---
+++ trled.c
@@ -0,0 +1,66 @@
+/**
+    Copyright (C) powturbo 2015
+    GPL v2 License
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    - email    : powturbo [AT] gmail.com
+    - github   : https://github.com/powturbo
+    - homepage : https://sites.google.com/site/powturbo/
+    - twitter  : https://twitter.com/powturbo
+
+    TurboRLE - "Efficient and fast Run Length Encoding"
+**/
+#include <string.h> 
+#include "trle.h"
+#include "trle_.h"
+         
+//------------------------------------- RLE with Escape char ------------------------------------------------------------------
+unsigned _srled(unsigned char *in, unsigned char *out, unsigned outlen, int e) {
+  unsigned char *ip = in, *op = out; int c;
+  while(op < out+outlen) { 
+    if((c = *ip++) != e) *op++ = c;
+	else { 
+	  int i; vbxget(ip, i);
+	  if(i) { c = *ip++; i+=3; rmemset(op, c, i); }
+	  else *op++ = e;
+    }
+  }
+  return ip - in;
+}
+
+unsigned srled(unsigned char *in, unsigned char *out, unsigned outlen) { return _srled(in+1, out, outlen, *in); }
+
+//------------------------------------- TurboRLE ------------------------------------------
+unsigned trled(unsigned char *in, unsigned char *out, unsigned outlen) {
+  unsigned char b[256]={0},*ip,*op=out;  
+  int m = -1,i,c; 
+  if(outlen < 1) return 0;
+
+  if(!*in) { memcpy(out,in+1,outlen); return outlen+1; }
+  if(*in++ == 1) return _srled(in+1, out, outlen, *in)+2;
+  
+  for(ip = in; ip < in+32; ip++)
+    for(i = 0; i < 8; ++i) 
+	  if(((*ip) >> i) & 1) b[(ip-in)<<3 | i] = ++m+1; 		
+		
+  while(op < out+outlen)
+    if(!(c=b[*ip])) *op++ = *ip++; 						
+	else { ip++; 
+	  vbzget(ip, i, m, c-1);
+	  c = *ip++; i+=3; rmemset(op,c,i); 					
+    }
+  return ip - in;
+}

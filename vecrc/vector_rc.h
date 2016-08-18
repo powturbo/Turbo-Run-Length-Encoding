da4dcc415d884d3f2cc0dd43cab67cb0b2f2ae59
powturbo <powturbo@gmail.com>
2016-08-18 17:53:14 +0200

    .

--- vecrc/vector_rc.h
+++
@@ -1,8 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-int vecrcenc(unsigned char *ip, int ilen, unsigned char *op);
-int vecrcdec(unsigned char *ip, int olen, unsigned char *op);
-#ifdef __cplusplus
-}
-#endif
